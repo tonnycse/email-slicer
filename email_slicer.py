356f221f41f607c78a881148dec3abba71856f21
tonnycse <tonnyalam98@gmail.com>
2023-05-17 23:27:00 -0700

    first commit

---
+++ email_slicer.py
@@ -0,0 +1,15 @@
+def main():
+    print("Welcome to the Email Slicer")
+    print("")
+
+    email_input = input("Input your email address: ")
+
+    (username, domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+
+    print("Username: ", username)
+    print("Domain : ", domain)
+    print("Extension: ", extension)
+
+while True:
+    main()
